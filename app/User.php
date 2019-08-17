e6b0a8d807db18549760b4f4e005b57938bc6674
EC2 Default User <ec2-user@ip-172-31-28-88.ap-southeast-1.compute.internal>
2019-08-17 01:24:04 +0000

    Add users_table_collumn for stripe

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, CanResetPassword, Billable;
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +36,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    protected $dates = ['trial_ends_at', 'subscription_ends_at'];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('stripe_subscription');
+            $table->string('stripe_plan');
             $table->rememberToken();
             $table->timestamps();
         });
