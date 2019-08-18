1d2c768e29aa1c46a013a2a3097ba7a89bc7e01b
EC2 Default User <business.ujinchu@gmail.com>
2019-08-18 05:10:09 +0000

    Prossecing Adding Subscription on SubscriptionController

--- app/Http/Controllers/SubscriptionsController.php
+++ app/Http/Controllers/SubscriptionsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class SubscriptionsController extends Controller
 {
     public function __construct()
