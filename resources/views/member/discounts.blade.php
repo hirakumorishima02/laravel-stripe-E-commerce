1c96cf95e8bb020031c124f5d3dc9f2e93a138da
EC2 Default User <business.ujinchu@gmail.com>
2019-08-27 13:08:56 +0000

    Complete discounts.blade.php

--- resources/views/member/discounts.blade.php
+++ resources/views/member/discounts.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.app')
-
 @section('content')
 
-<h1>Discounts Coupon Page</h1>
-<table class="table">
+<h4>Discounts Coupon Page</h4>
+<table class="table striped">
   <thead class="thead-dark">
     <tr>
       <th scope="col">Coupon</th>
       <th scope="col">Coupon ID</th>
+      <th scope="col">Value</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>初回購入割引</td>
-      <td>MFcT2qRs</td>
+      <td><input value="MFcT2qRs" onclick="this.select(0,this.value.length)"></td>
+      <td>10% Descount</td>
     </tr>
     <tr>
       <td>2回目購入割引</td>
-      <td>22IYuWhq</td>
+      <td><input value="22IYuWhq" onclick="this.select(0,this.value.length)"></td>
+      <td>5% Descount</td>
     </tr>
     <tr>
       <td>1周年記念割引</td>
-      <td>YUF9Gmtc</td>
+      <td><input value="YUF9Gmtc" onclick="this.select(0,this.value.length)"></td>
+      <td>5% Descount</td>
     </tr>
   </tbody>
 </table>
