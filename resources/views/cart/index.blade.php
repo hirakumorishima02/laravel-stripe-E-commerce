fe3d3aea39b8d0abe695a88b406f218dc09e5a1c
EC2 Default User <business.ujinchu@gmail.com>
2019-08-28 02:18:10 +0000

    Before Edit cart/index.blade form

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -24,41 +24,84 @@
   <div class="payment-errors alert alert-danger"
     style="display: none;">
   </div>
+<div class='subscription-forms'>
   {!! Form::open([
-    'route' => 'cart.complete',
+    'route' => 'plans.process',
     'class' => 'form',
     'id' => 'purchase-form'
   ]) !!}
-    <div class="form-group">
-      <div class="row">
-        <div class="col-xs-12">
-          <label for="card-number" class="control-label">
-            Credit Card Number
-          </label>
-        </div>
-        <div class="col-sm-4">
-          <input type="text"
-            class="form-control"
-            id="card-number"
-            name="cardnumber"
-            placeholder="Valid Card Number"
-            required autofocus data-stripe="number"
-            value="
-            {{ App::environment() == 'local' ? '4242424242424242' : '' }}
-            ">
-        </div>
-      </div>
+<div class="row">
+  <div class="col-xs-12">
+    <label for="billing_name" class="control-label">
+      Billing Name
+    </label>
+  </div>
+  <div class="col s4 ">
+    <input type="text"
+      class="form-control"
+      id="billing_name"
+      required autofocus data-stripe="billing_name"
+      value="John Williams">
+  </div>
+</div>
+<div class='row'>
+    <div class='col s4 m3'>
+            <label for="billing_address">Billing Adress</label>
+            <input type="text" size="3"
+              class="form-control"
+              name="billing_address"
+              data-stripe="billing_address"
+              id="billing_address"
+              value="24 Wholley st"
+              required>
     </div>
-    <div class="form-group">
-      <div class="row">
-        <div class="col-xs-4">
-          <label for="card-month">Expiration Date</label>
-        </div>
-        <div class="col-xs-8">
-          <label for="card-cvc">Security Code</label>
-        </div>
-      </div>
-      <div class="row">
+      <div class='col s4 m4'>
+            <label for="card-month">Billing City</label>
+            <input type="text" size="4"
+              class="form-control"
+              name="billing_city" data-stripe="billing_city"
+              id="billing_city"
+              value="Bayswater Perth"
+              required>
+    </div>
+    <div class='col s4 m2'>
+            <label for="billing_zip" class="billing_zip">
+              Billing Zip
+            </label>
+            <input type="text"
+              class="form-control"
+              id="billing_zip"
+              placeholder="Coupon Code"
+              autofocus data-stripe="billing_zip"
+              name="billing_zip"
+              value="6053">
+    </div>
+    <div class='col s4 m2'>
+            <label for="billing_state">Billing State</label>
+            <input type="text"
+              class="form-control" id="billing_state"
+              data-stripe="billing_state"
+              size="6"
+              value="Australlia"
+              required
+              >
+    </div>
+</div>
+<div class="col-xs-12">
+  <label for="card-number" class="control-label">
+    Credit Card Number
+  </label>
+</div>
+<div class="col s4 ">
+  <input type="text"
+    class="form-control"
+    id="card-number"
+    placeholder="Valid Card Number"
+    required autofocus data-stripe="number"
+    value="4242424242424242">
+</div>
+</div>
+<div class='row'>
         <div class="col-xs-2">
           <input type="text" size="3"
             class="form-control"
@@ -68,133 +111,38 @@
             id="card-month"
             value="{{ App::environment() == 'local' ? '12' : '' }}"
             required>
-        </div>
-        <div class="col-xs-2">
-          <input type="text" size="4"
-            class="form-control"
-            name="exp_year" data-stripe="exp-year"
-            placeholder="YYYY" id="card-year"
-            value="{{ App::environment() == 'local' ? '2020' : '' }}"
-            required>
-        </div>
-        <div class="col-xs-2">
-          <input type="text"
-            class="form-control" id="card-cvc"
-            placeholder=""
-            size="6"
-            value="{{ App::environment() == 'local' ? '111' : '' }}"
-            >
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <label for="billing_name" class="control-label">
-            Billing Name
-          </label>
-        </div>
-        <div class="col-sm-4">
-          <input type="text"
-            class="form-control"
-            id="billing_name"
-            placeholder="John Williams"
-            autofocus data-stripe="number"
-            name="billing_name"
-            value="
-            {{ App::environment() == 'local' ? 'John Williams' : '' }}
-            ">
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <label for="billing_address" class="control-label">
-            Billing Address
-          </label>
-        </div>
-        <div class="col-sm-4">
-          <input type="text"
-            class="form-control"
-            id="billing_address"
-            placeholder="24"
-            autofocus data-stripe="number"
-            name="billing_address"
-            value="
-            {{ App::environment() == 'local' ? '24' : '' }}
-            ">
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <label for="billing_city" class="control-label">
-            Billing City
-          </label>
-        </div>
-        <div class="col-sm-4">
-          <input type="text"
-            class="form-control"
-            id="billing_city"
-            placeholder="Bayswater Perth"
-            autofocus data-stripe="number"
-            name="billing_city"
-            value="
-            {{ App::environment() == 'local' ? 'Bayswater Perth' : '' }}
-            ">
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <label for="billing_state" class="control-label">
-            Billing State
-          </label>
-        </div>
-        <div class="col-sm-4">
-          <input type="text"
-            class="form-control"
-            id="billing_state"
-            placeholder="Westan Australlia"
-            autofocus data-stripe="number"
-            name="billing_state"
-            value="
-            {{ App::environment() == 'local' ? 'Westan Australlia' : '' }}
-            ">
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <label for="billing_zip" class="control-label">
-            Billing Zip
-          </label>
-        </div>
-        <div class="col-sm-4">
-          <input type="text"
-            class="form-control"
-            id="billing_zip"
-            placeholder="6053"
-            autofocus data-stripe="number"
-            name="billing_zip"
-            value="
-            {{ App::environment() == 'local' ? '6053' : '' }}
-            ">
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <label for="billing_country" class="control-label">
-            Billing Country
-          </label>
-        </div>
-        <div class="col-sm-4">
-          <input type="text"
-            class="form-control"
-            id="billing_country"
-            placeholder="Australlia"
-            autofocus data-stripe="number"
-            name="billing_country"
-            value="
-            {{ App::environment() == 'local' ? 'Australlia' : '' }}
-            ">
-        </div>
-      </div>
-    </div>
+  <div class='col s4 m2'>
+        <label for="card-month">Expiration Year</label>
+        <input type="text" size="4"
+          class="form-control"
+          name="exp_year" data-stripe="exp-year"
+          placeholder="YYYY" id="card-year"
+          value="2020"
+          required>
+</div>
+<div class='col s4 m2'>
+        <label for="card-cvc">Security Code</label>
+        <input type="text"
+          class="form-control" id="card-cvc"
+          placeholder=""
+          size="6"
+          value="111"
+          required
+          >
+</div>
+<div class='col s4 m3'>
+        <label for="coupon" class="control-label">
+          Coupon Code
+        </label>
+        <input type="text"
+          class="form-control"
+          id="coupon"
+          placeholder="Coupon Code"
+          autofocus data-stripe="number"
+          name="coupon"
+          value="MFcT2qRs">
+</div>
+</div>
     <div class="center-block form-actions">
       <button type="submit" class="submit-button btn btn-primary btn-lg">
         Complete Order
